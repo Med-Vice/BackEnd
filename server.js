4ccb2d79fa1b5dbe921c7210dbac96e23288a876
Abdi Mohamud <abdinajibmohamud@gmail.com>
2020-11-28 02:56:47 -0800

    fixed error on deployment

--- server.js
+++ server.js
@@ -6,25 +6,31 @@ const app = express();
 
 
 
-app.get('/search', (request, response) => {
+app.get('/search', async (request, response) => {
     //Do something when someone makes request to localhost:3000/search
     //request parameter - information about the request coming in
    //response parameter - response object that we can use to send a response
-   const searchQuery = request.query.searchquery;
-   if(searchQuery != null){
-    searchNLM(searchQuery)
-    .then(results => {
-        //Returns a 200 Status OK with Results JSON back to the client.
-        response.status(200);
-        response.json(results);
-    });
+   try{
+       const searchQuery = request.query.searchquery;
+        if(searchQuery != null){
+            const results =searchNLM(searchQuery)
+        
+                //Returns a 200 Status OK with Results JSON back to the client.
+                response.status(200).json({data:results});
+            
+        } 
 
-}else{
-  response.end();
-}
+   }catch(error){
+    response.status(404).json({message:error});
+   }
+
+
 });
 //Catches requests made to localhost:3000/
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Welcome to the Books API",server: 'up and running!', port: `${process.env.PORT}`, environment: `${process.env.NODE_ENV}`})
+  });
 
 
 //Initialises the express server on the port 30000
