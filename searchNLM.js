6b55a7ca2c6de76bfa45e3dc43ac10d2bd0fe41c
Abdi Mohamud <abdinajibmohamud@gmail.com>
2020-11-28 11:25:48 -0800

    debug

--- searchNLM.js
+++ searchNLM.js
@@ -2,24 +2,31 @@ const puppeteer = require('puppeteer');
 
 
 const searchNLM = async (searchQuery) => {
-    const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    await page.goto(`https://wsearch.nlm.nih.gov/ws/query?db=digitalCollections&term=${searchQuery}`);
-    await page.waitForSelector('body > div.pretty-print');
-    const textContent = await page.evaluate(() => {
-        const result = []
-         const res = document.querySelectorAll('document');
-         Array.from(res).forEach(function (el) { 
-            result.push(el.innerHTML)
-        })
-         return result
-         
-     });
 
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
-    return textContent;
+    try{
+
+        const browser = await puppeteer.launch();
+    
+        const page = await browser.newPage();
+        await page.goto(`https://wsearch.nlm.nih.gov/ws/query?db=digitalCollections&term=${searchQuery}`);
+        await page.waitForSelector('body > div.pretty-print');
+        const textContent = await page.evaluate(() => {
+            const result = []
+             const res = document.querySelectorAll('document');
+             Array.from(res).forEach(function (el) { 
+                result.push(el.innerHTML)
+            })
+             return result
+             
+         });
+    
+        await page.screenshot({path: 'example.png'});
+        await browser.close();
+        return textContent;
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 module.exports = searchNLM;
