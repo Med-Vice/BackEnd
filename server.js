4f5708321ecb9a0c98b574d34cf37e939299578b
abdimohamud <abdinajibmohamud@gmail.com>
2020-11-28 23:53:16 -0800

    working

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const helmet = require("helmet");
 
 const puppeteer = require('puppeteer-core');
 const searchNLM = require('./searchNLM.js')
-const app = express();
-app.use(helmet());
-app.use(cors());
+const server = express();
+server.use(express.json())
+server.use(helmet());
+server.use(cors());
 
 
 
-app.get('/search', async (request, response) => {
+server.get('/search', async (request, response) => {
     //Do something when someone makes request to localhost:3000/search
     //request parameter - information about the request coming in
    //response parameter - response object that we can use to send a response
@@ -35,10 +36,10 @@ app.get('/search', async (request, response) => {
 });
 //Catches requests made to localhost:3000/
 
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
     res.status(200).json({message: "Welcome to the MedVice API",server: 'up and running!', port: `${process.env.PORT}`, environment: `${process.env.NODE_ENV}`})
   });
 
 
 //Initialises the express server on the port 30000
-module.exports = app;
+module.exports = server;
